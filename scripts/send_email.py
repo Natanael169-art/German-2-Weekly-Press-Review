e1b0763e2e8a373995efbce6aedff9d9f7259020
Natanael169-art <natanael.farret@hotmail.com>
2025-10-06 11:57:02 +0200

    Update send_email.py

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -22,6 +22,7 @@ email_data = {
         {
             "to": [
                 {"email": "natanael.farret@hertz.com"},
+                {"email": "GMarx@hertz.com"}
             ],
             "subject": "Gunhild Weekly Press Review"
         }
